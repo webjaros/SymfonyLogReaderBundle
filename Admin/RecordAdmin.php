904c39244274ada8b6ea41d48166c04f10fb2dc1
Jaroslavs Kuprijanovs <webjaros@gmail.com>
2017-03-06 21:30:46 +0000

    bugfix: default sort column

--- Admin/RecordAdmin.php
+++ Admin/RecordAdmin.php
@@ -14,7 +14,7 @@ class RecordAdmin extends AbstractAdmin
     protected $datagridValues = [
         '_page' => 1, // Display the first page (default = 1)
         '_sort_order' => 'DESC', // Descendant ordering (default = 'ASC')
-        '_sort_by' => 'periodDate' // name of the ordered field (default = the model id field, if any)
+        '_sort_by' => 'createdAt' // name of the ordered field (default = the model id field, if any)
         // the '_sort_by' key can be of the form 'mySubModel.mySubSubModel.myField'.
     ];
 
